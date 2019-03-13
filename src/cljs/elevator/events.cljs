ec948305dbaff2ba5b2f07a90076d142099df9bd
benpalin <benpalin1@gmail.com>
2019-03-13 11:25:20 -0500

    Broke logic operations for events into their own functions and added doc-strings

--- src/cljs/elevator/events.cljs
+++ src/cljs/elevator/events.cljs
@@ -33,7 +33,9 @@
     (assoc db :common/error error)))
 
 (defn get-next-stop
-  "Takes the set of selected floors, the current floor the elevator is at, and the direction the elevator is going, and returns the next floor the elevator will stop at"
+  "Takes the set of selected floors, the current floor the elevator is at,
+  and the direction the elevator is going, and returns the next floor the
+  elevator will stop at"
   [floor-set current-floor direction]
   (if (= direction :up)
     (->> floor-set
@@ -45,7 +47,17 @@
          (sort)
          (last))))
 
-
+(defn in-motion-new-direction
+  "Takes the state db, and returns the direction the elevator should now be going.
+  This should only be used when the elevator is currently in motion"
+  [{:keys [direction current-floor selected-floors]}]
+  (if (= :up direction)
+    (if (seq (filter #(> % current-floor) selected-floors))
+      :up
+      :down)
+    (if (seq (filter #(< % current-floor) selected-floors))
+      :down
+      :up)))
 
 (rf/reg-event-fx
   :open-doors
@@ -58,13 +70,7 @@
   (fn [{:keys [db]}]
     (let [{:keys [selected-floors direction current-floor]} db]
       (if (seq selected-floors)
-        (let [new-direction (if (= :up direction)
-                              (if (seq (filter #(> % current-floor) selected-floors))
-                                :up
-                                :down)
-                              (if (seq (filter #(< % current-floor) selected-floors))
-                                :down
-                                :up))
+        (let [new-direction (in-motion-new-direction db)
               next-stop (get-next-stop selected-floors current-floor new-direction)]
           {:db (assoc db :door-status :closed :direction new-direction :next-stop next-stop)
            :dispatch [:move-floor new-direction]})
@@ -75,26 +81,42 @@
   (fn [{:keys [db]} [_ direction]]
     {:dispatch-later [{:ms 1000 :dispatch [:increase-floor direction]}]}))
 
+(defn at-next-stop?
+  "Takes the state db and returns true if the elevator has arrived at the next
+  stop, false if it has not"
+  [db]
+ (= (:next-stop db) (:current-floor db)))
+
+(defn direction-fn
+  "Takes a direction the elevator is moving and returns the correct function needed to change the current
+  floor as it moves."
+  [direction]
+  (if (= :up direction) inc dec))
+
 (rf/reg-event-fx
   :increase-floor
   (fn [{:keys [db]} [_ direction]]
-    (let [direction-fn (if (= :up direction) inc dec)]
-      (if (not= (:next-stop db) (:current-floor db))
+    (let [direction-fn (direction-fn direction)]
+      (if (not (at-next-stop? db))
         {:db (update db :current-floor direction-fn)
          :dispatch [:move-floor direction]}
         {:db (update db :selected-floors #(disj % (:current-floor db)))
          :dispatch [:open-doors]}))))
 
+(defn standing-still-new-direction
+  "Takes the current-floor and the newly-selected floor and returns the direction
+  the elevator should move. Only to be used when the elevator is standing still."
+  [current-floor new-floor]
+  (if (> new-floor current-floor)
+    :up
+    :down))
+
 (rf/reg-event-fx
   :select-new-floor
   (fn [{:keys [db]} [_ floor]]
-    (let [{:keys [selected-floors in-motion? direction current-floor in-motion?]} db
+    (let [{:keys [selected-floors current-floor in-motion?]} db
           selected-floors (conj selected-floors floor)
-          new-direction (if in-motion?
-                          direction
-                          (if (> floor current-floor)
-                            :up
-                            :down))]
+          new-direction (standing-still-new-direction current-floor floor)]
       (if-not in-motion?
         {:db (assoc db :selected-floors selected-floors
                        :in-motion? true
